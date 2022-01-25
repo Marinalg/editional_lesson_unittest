f34e30f326ca113a24dfec316e5a0c5110017061
marina <hyawey2017@gmail.com>
2022-01-25 22:59:47 +0200

    homework_unittest_for homework_9 additional lesson _2

--- homework_9_unnittest/logger.py
+++ homework_9_unnittest/logger.py
@@ -1,10 +1,15 @@
 import logging
 import sys
+import os
 
 log = logging.getLogger("MYFIRSTLOGGERFORHOMEWORK")
-log.level = logging.INFO
+log.level = logging.DEBUG
 stream_handler = logging.StreamHandler(sys.stdout)
 log.addHandler(stream_handler)
+here = os.path.dirname(os.path.abspath(__file__))
+filename = os.path.join(here, 'apply.log')
+file_handler = logging.FileHandler(filename, encoding= "utf8")
+log.addHandler(file_handler)
 
 
 
